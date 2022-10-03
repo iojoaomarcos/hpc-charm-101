60d748c06e97fcabba6446d1dd239fb713e749fa
iojoaomarcos <joao.marcossantos@outlook.com>
2022-10-03 19:47:28 +0000

    matmul with blas

---
+++ matmul_cblas/run_cmds.sh
@@ -0,0 +1,4 @@
+/home/ubuntu/environment/charm-v7.0.0/bin/charmc matmul.ci
+/home/ubuntu/environment/charm-v7.0.0/bin/charmc matmul.C
+make
+/home/ubuntu/environment/charm-v7.0.0/bin/charmrun matmul +p4 3 3 
